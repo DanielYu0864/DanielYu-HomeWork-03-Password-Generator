cb4f0e5dcd27cfd8b3aeb8245cdcbd7997e94134
Daniel.Yu <adam741963@gmail.com>
2020-07-03 12:25:20 -0700

    add copybut

---
+++ README.md
@@ -0,0 +1,9 @@
+# 03 JavaScript: Password Generator
+
+### what I did for homework
+
+1. created variables for each character and button.
+2. make getRandom() function.
+3. make generatePassowrd() function.
+4. input the random password to the textarea.
+5. make the copy button worked.
--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Assignment Code
+// give the variables
 var generateBtn = document.querySelector("#generate");
 var hasSpecialCharacters;
 var hasLowerCharacters;
 var hasUpperCharacters;
 var hasNumberCharacters;
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["~", "!", "?", "/", "@", "#", "$", "%", "^", "&", "*", "+", "-", "=", "(", ")", "[", "]", "{", "}", "_", ";", ":", "'", "\"", "|"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// variables for each characters
+var numericCharacters = ["0123456789"];
+var specialCharacters = ["~!?/@#$%^&*+-=()[]{}_;:'\"|"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var lowerCase = upperCase.toString().toLowerCase().split(",");
+// give copy button var
+var clipboardEl = document.querySelector("#copy-button");
 
-
+//function to make randomly selected
 function getRandom(arr) {
   var randIndex = Math.floor(Math.random() * arr.length);
   console.log(randIndex, arr);
@@ -17,73 +21,62 @@ function getRandom(arr) {
   return randElement;
 }
 
-
 function generatePassword() {
-  var passwordlent = prompt("how many characters you want in your password?");
-  var newPassword = [""];
+  var passwordlent = prompt("How many characters do you want in your password?");
+  var newPassword = "";
+  var randomPassowrd = [];
 
-
+  // first question
   if (passwordlent < 8 || passwordlent > 128) {
     alert("please type 8 ~ 128 characters");
-    passwordlent = prompt("how many characters you want in your password?");
+    passwordlent = prompt("How many characters do you want in your password?");
   }
-
+  // secound question
   hasSpecialCharacters = confirm(
     "Click OK to confirm including special characters."
   );
-
+  // thrid question
   hasLowerCharacters = confirm(
     "Click OK to confirm including lower characters."
   );
-
+  // fourth question
   hasUpperCharacters = confirm(
     "Click OK to confirm including upper characters."
   );
-
-
+  // fifth question
   hasNumberCharacters = confirm(
-    "Click OK to confirm including numberic characters."
+    "Click OK to confirm including numeric characters."
   );
-
+  // make sure user choose at least one of options
   if (hasSpecialCharacters === false && hasLowerCharacters === false && hasUpperCharacters === false && hasNumberCharacters === false) {
-    alert ("You need to chooise at least one")
+      alert ("You need to choose at least one")
+    // secound question
     hasSpecialCharacters = confirm(
       "Click OK to confirm including special characters."
     );
+    // thrid question
     hasLowerCharacters = confirm(
       "Click OK to confirm including lower characters."
     );
+    // fourth question
     hasUpperCharacters = confirm(
       "Click OK to confirm including upper characters."
     );
+    // fifth question
     hasNumberCharacters = confirm(
-      "Click OK to confirm including numberic characters."
+      "Click OK to confirm including numeric characters."
     );
   }
 
+  if (hasSpecialCharacters) newPassword = newPassword.concat(specialCharacters);
+    if (hasLowerCharacters) newPassword = newPassword.concat(lowerCase);
+    if (hasUpperCharacters) newPassword = newPassword.concat(upperCase);
+    if (hasNumberCharacters) newPassword = newPassword.concat(numericCharacters);
 
-
-  var charactersCount = hasNumberCharacters + hasLowerCharacters + hasUpperCharacters + hasSpecialCharacters;
-
-  // console.log(charactersCount);
-  for (var i = 0; i < passwordlent; i+= charactersCount) {
-
-    if (hasSpecialCharacters) {
-      newPassword = newPassword.concat(getRandom(specialCharacters));
-    }
-    if (hasLowerCharacters) {
-      newPassword = newPassword.concat(getRandom(lowerCase));
-    }
-    if (hasUpperCharacters) {
-      newPassword = newPassword.concat(getRandom(upperCase));
-    }
-    if (hasNumberCharacters) {
-      newPassword = newPassword.concat(getRandom(numericCharacters));
-    }
+  for (var i = 0; i < passwordlent; i++ ) {
+    randomPassowrd = randomPassowrd.concat(getRandom(newPassword));
   }
-  return newPassword;
-
-
+  return randomPassowrd.join("");
 
 }
 
@@ -92,19 +85,17 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  // function getFinalPassword() {
-
-  //   for (j = 0; j < passwordlent; j++) {
-  //     password = getRandom(password);
-  //   }
-  //   return password ;
-
-  // };
-  // var finalPassword = getFinalPassword();
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// add function for copy to clipboard
+clipboardEl.addEventListener("click", function () {
+  var copyTextarea = document.getElementById("password");
+  copyTextarea.select();
+  document.execCommand("copy");
+  alert("copied password");
+});
